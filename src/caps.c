8bc46387ff5cf79266a33b2b2229dce3b142fee0
spire134 <spire134@gmail.com>
2025-08-19 20:54:58 -0700

    Cleanup2
    More preedit changes

--- src/caps.c
+++ src/caps.c
@@ -10,14 +10,14 @@ u32 GetCurrentLevelCap(void)
     static const u32 sLevelCapFlagMap[][2] =
     {
         {FLAG_BADGE01_GET, 15},
-        {FLAG_BADGE02_GET, 19},
-        {FLAG_BADGE03_GET, 24},
-        {FLAG_BADGE04_GET, 29},
-        {FLAG_BADGE05_GET, 31},
-        {FLAG_BADGE06_GET, 33},
-        {FLAG_BADGE07_GET, 42},
-        {FLAG_BADGE08_GET, 46},
-        {FLAG_IS_CHAMPION, 58},
+        {FLAG_BADGE02_GET, 20},
+        {FLAG_BADGE03_GET, 30},
+        {FLAG_BADGE04_GET, 40},
+        {FLAG_BADGE05_GET, 50},
+        {FLAG_BADGE06_GET, 60},
+        {FLAG_BADGE07_GET, 70},
+        {FLAG_BADGE08_GET, 80},
+        {FLAG_IS_CHAMPION, 90},
     };
 
     u32 i;
