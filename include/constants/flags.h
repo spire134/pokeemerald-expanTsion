8161181b96ada790dc4862748cd86e612e5e34c6
spire134 <spire134@gmail.com>
2025-09-01 19:07:01 -0700

    intro once again

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -43,13 +43,13 @@
 #define TEMP_FLAGS_END   FLAG_TEMP_1F
 #define NUM_TEMP_FLAGS   (TEMP_FLAGS_END - TEMP_FLAGS_START + 1)
 
-#define FLAG_UNUSED_0x020    0x20 // Unused Flag
-#define FLAG_UNUSED_0x021    0x21 // Unused Flag
-#define FLAG_UNUSED_0x022    0x22 // Unused Flag
-#define FLAG_UNUSED_0x023    0x23 // Unused Flag
-#define FLAG_UNUSED_0x024    0x24 // Unused Flag
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
-#define FLAG_UNUSED_0x026    0x26 // Unused Flag
+#define FLAG_NOTE_READ       0x20 // Unused Flag
+#define FLAG_MET_RANGER_A    0x21 // Unused Flag
+#define FLAG_MET_RANGER_B    0x22 // Unused Flag
+#define FLAG_MET_RANGER_SOLO 0x23 // Unused Flag
+#define FLAG_GOT_STARTER     0x24 // Unused Flag
+#define FLAG_STORM_STARTED   0x25 // Unused Flag
+#define FLAG_SHOREFALL_INTRO 0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
