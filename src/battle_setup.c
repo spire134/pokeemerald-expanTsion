bbcbe8821c04cb2e17db428c7c1892718d58babf
spire134 <spire134@gmail.com>
2025-09-13 15:38:18 -0700

    starterop

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -869,11 +869,11 @@ void ChooseStarter(void)
 
 static void CB2_GiveStarter(void)
 {
-    u16 starterMon;
+//    u16 starterMon;
 
-    *GetVarPointer(VAR_STARTER_MON) = gSpecialVar_Result;
-    starterMon = GetStarterPokemon(gSpecialVar_Result);
-    ScriptGiveMon(starterMon, 5, ITEM_NONE);
+  //  *GetVarPointer(VAR_STARTER_MON) = gSpecialVar_Result;
+    //starterMon = GetStarterPokemon(gSpecialVar_Result);
+    //ScriptGiveMon(starterMon, 5, ITEM_NONE);
     ResetTasks();
     PlayBattleBGM();
     SetMainCallback2(CB2_StartFirstBattle);
