1edf0f955ebcbc8fd6d58f6ee7edb41f50ca0cbc
spire134 <spire134@gmail.com>
2025-09-22 23:29:08 -0700

    Mara and others

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -667,7 +667,7 @@
 #define FLAG_MARA_EVAL_READY            0x26B // Unused Flag
 #define FLAG_MARA_DEFEATED              0x26C // Unused Flag
 #define FLAG_RANGERS_REASSIGNED         0x26D // Unused Flag
-#define STATIC_DRONE_FIGHT  0x26E // Unused Flag
+#define FLAG_STATIC_DRONE_FIGHT         0x26E // Unused Flag
 #define FLAG_UNUSED_0x26F  0x26F // Unused Flag
 #define FLAG_UNUSED_0x270  0x270 // Unused Flag
 #define FLAG_UNUSED_0x271  0x271 // Unused Flag
