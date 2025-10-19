706b206f899303d20dd34e5437162b3929e3a953
spire134 <spire134@gmail.com>
2025-10-19 13:15:12 -0700

    peacetime and defined flags

--- src/field_specials.c
+++ src/field_specials.c
@@ -1209,7 +1209,7 @@ void EndLotteryCornerComputerEffect(void)
 void SetTrickHouseNuggetFlag(void)
 {
     u16 *specVar = &gSpecialVar_0x8004;
-    u16 flag = FLAG_HIDDEN_ITEM_TRICK_HOUSE_NUGGET;
+    u16 flag = FLAG_HIDDEN_ITEM_SHUNU_BEACH_POKE_BALL;
     *specVar = flag;
     FlagSet(flag);
 }
@@ -1217,7 +1217,7 @@ void SetTrickHouseNuggetFlag(void)
 void ResetTrickHouseNuggetFlag(void)
 {
     u16 *specVar = &gSpecialVar_0x8004;
-    u16 flag = FLAG_HIDDEN_ITEM_TRICK_HOUSE_NUGGET;
+    u16 flag = FLAG_HIDDEN_ITEM_SHUNU_BEACH_POKE_BALL;
     *specVar = flag;
     FlagClear(flag);
 }
