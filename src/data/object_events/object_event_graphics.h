682376f9622e8e69f22dadd4956eaf9293ce2781
spire134 <spire134@gmail.com>
2025-12-14 22:30:36 -0700

    Update object_event_graphics.h

--- src/data/object_events/object_event_graphics.h
+++ src/data/object_events/object_event_graphics.h
@@ -1,3 +1,6 @@
+// Include Pokemon graphics for follower Pokemon sprites
+#include "data/graphics/pokemon.h"
+
 const u32 gObjectEventPic_BrendanNormalRunning[] = INCBIN_U32("graphics/object_events/pics/people/brendan/walking.4bpp", "graphics/object_events/pics/people/brendan/running.4bpp");
 const u16 gObjectEventPal_Brendan[] = INCBIN_U16("graphics/object_events/palettes/brendan.gbapal");
 const u32 gObjectEventPic_RubySapphireBrendanNormal[] = INCBIN_U32("graphics/object_events/pics/people/ruby_sapphire_brendan/walking.4bpp");
